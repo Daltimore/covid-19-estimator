fa0e5c0b0826666a4cd7eef1d314b043afe9a0fe
Daltimore <obinnaleo78@gmail.com>
2020-04-09 04:04:31 +0100

    challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,33 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const input = {
+    region: {
+      name: 'Africa',
+      avgAge: 19.7,
+      avgDailyIncomeInUSD: 5,
+      avgDailyIncomePopulation: 0.71
+    },
+    periodType: 'days',
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+  };
+  return {
+    data,
+    impact: {
+      currentlyInfected: () => input.reportedCases * 10,
+      get infectionsByRequestedTime() {
+        return `${this.currentlyInfected}` * 1024;
+      }
+    },
+    severeImpact: {
+      currentlyInfected: () => input.reportedCases * 50,
+      get infectionsByRequestedTime() {
+        return `${this.currentlyInfected}` * 1024;
+      }
+    }
+  };
+};
+
 
 export default covid19ImpactEstimator;
